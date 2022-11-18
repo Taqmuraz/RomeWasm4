a9ab02fc1fab9da79d132eb18b48865ebc1457ec
taimuraz.fardzinov <taimuraz.fardzinov@aspose.com>
2022-11-18 16:10:46 +0100

    correct letter drawing

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #define screenWidth 160
 #define screenHeight 160 
 
-StaticPointer<Array<byte, 8>> literal = staticNew(Array<byte, 8>{
+StaticPointer<Array<byte, 8>> literalR = staticNew(Array<byte, 8>{
     0b01010101,
     0b01000001,
     0b01000001,
@@ -17,10 +17,33 @@ StaticPointer<Array<byte, 8>> literal = staticNew(Array<byte, 8>{
     0b00110010,
     0b10000011,
 });
+StaticPointer<Array<byte, 8>> literalL = staticNew(Array<byte, 8>{
+    0b00000001,
+    0b00000001,
+    0b00000001,
+    0b00000001,
+    0b00000001,
+    0b00000001,
+    0b00000001,
+    0b01010101,
+});
 byte* frame = (byte*)0xa0;
+
+void drawLiteral(Vec2 pos, StaticPointer<Array<byte, 8>> literal)
+{
+    for (uint i = 0; i < 8; i++)
+    {
+        int p = (int)pos.x + ((int)i + (int)pos.y) * screenWidth;
+        int shift = (p & 3) << 1;
+        frame[p >> 2] = (byte)(((int)(*literal)[i]) << shift);
+        frame[(p >> 2) + 1] = (byte)(((int)(*literal)[i]) >> (8 - shift));
+    }
+}
 
 void update () 
 {
-    Vec2 pos = Vec2(15, 15);
-    for (uint i = 0; i < 8; i++) frame[(int)pos.x + ((int)i + (int)pos.y) * (screenWidth >> 2)] = (*literal)[i];
+    Vec2 posR = Vec2(15, 15);
+    Vec2 posL = Vec2(25, 15);
+    drawLiteral(posR, literalR);
+    drawLiteral(posL, literalL);
 }
